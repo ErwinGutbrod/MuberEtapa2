8371832370c7a90e2bb912d7c6f6d66d60e11924
Andrea Gómez Del Mónaco <andrea.gomez@despegar.com>
2017-04-24 16:02:22 -0300

    Ca

--- MuberRESTfulGrupo18/src/main/java/bd2/web/MuberRestController.java
+++ MuberRESTfulGrupo18/src/main/java/bd2/web/MuberRestController.java
@@ -1,17 +1,12 @@
 package bd2.web;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,10 +29,43 @@ public class MuberRestController {
 		return session;
 	}
 
-	@RequestMapping(value = "/example", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
-	public String pasajeros() {
+	@RequestMapping(value = "/services/pasajeros", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
+	public String getPassengers() {
+		Map<String, Object> aMap = new HashMap<String, Object>();
+		aMap.put("result", "OK");
+		return new Gson().toJson(aMap);
+	}
+	
+	@RequestMapping(value = "/services/viajes/abiertos", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
+	public String getTravelOpened() {
+		Map<String, Object> aMap = new HashMap<String, Object>();
+		aMap.put("result", "OK");
+		return new Gson().toJson(aMap);
+	}
+	
+	@RequestMapping(value = "/services/conductores", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
+	public String getDrivers() {
+		Map<String, Object> aMap = new HashMap<String, Object>();
+		aMap.put("result", "OK");
+		return new Gson().toJson(aMap);
+	}
+	
+	@RequestMapping(value = "/services/conductores/detalle", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
+	public String getDriverDetail(@RequestParam(value="conductorId", required = true) String conductorId) {
+		Map<String, Object> aMap = new HashMap<String, Object>();
+		aMap.put("result", "OK");
+		return new Gson().toJson(aMap);
+	}
+	
+	@RequestMapping(value = "/services/viajes/nuevo", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
+	public String getNewTravel(@RequestParam(value="conductorId", required = true) String conductorId,
+			@RequestParam(value="origen", required = true) String origen,
+			@RequestParam(value="destino", required = true) String destino,
+			@RequestParam(value="conductorId", required = true) String cantidadPasajeros) {
 		Map<String, Object> aMap = new HashMap<String, Object>();
 		aMap.put("result", "OK");
 		return new Gson().toJson(aMap);
 	}
 }
+
+
