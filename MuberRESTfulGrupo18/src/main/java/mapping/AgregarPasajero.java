ee67c4b092f1e3c7de7c4b87862d466af2f46a32
Andrea <agdelmonaco@gmail.com>
2017-05-21 16:53:40 -0300

    Cambios

---
+++ MuberRESTfulGrupo18/src/main/java/mapping/AgregarPasajero.java
@@ -0,0 +1,23 @@
+package mapping;
+
+
+public class AgregarPasajero {
+	private int viajeId;
+	private int pasajeroId;
+	
+	public int getViajeId() {
+		return viajeId;
+	}
+	public void setViajeId(int viajeId) {
+		this.viajeId = viajeId;
+	}
+	public int getPasajeroId() {
+		return pasajeroId;
+	}
+	public void setPasajeroId(int pasajeroId) {
+		this.pasajeroId = pasajeroId;
+	}
+	
+	
+}
+
